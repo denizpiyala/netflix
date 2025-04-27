b9ffc673c376a8db6f6139d00f9ca3c76b5e2dab
denizpiyala <159885392+denizpiyala@users.noreply.github.com>
2025-04-27 16:58:45 +0300

    Implement login component with dynamic sign-in/sign-up functionality and styling

--- src/pages/Home/Login/Login.jsx
+++ src/pages/Home/Login/Login.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
-import './Login.scss'
-import logo from '../../../assets/logo.png'
+import React, { useState } from "react";
+import "./Login.scss";
+import logo from "../../../assets/logo.png";
 
 const Login = () => {
+  const [signState, setSignState] = useState("sign in");
   return (
-    <div className='Login'>
-      <img src={logo} alt="" className='login-logo' />
+    <div className="login">
+      <img src={logo} alt="" className="login-logo" />
       <div className="login-form">
-        <h1>Sign In</h1>
-        <form >
-          <input type='text' placeholder='Your Name'/>
-          <input type='email' placeholder='Email'/>
-          <input type='password' placeholder='Password'/>
-          <button>Sign In</button>
+        <h1>{signState}</h1>
+        <form>
+          {signState === "sign up" ? (
+            <input type="text" placeholder="Your Name" />
+          ) : (
+            <></>
+          )}
+
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
-              <input type='checkbox' />
-              <label htmlFor=''>Remember Me</label>
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
             </div>
             <p>Need Help</p>
           </div>
         </form>
+        <div className="form-switch">
+          {signState === "sign in" ? (
+            <p>
+              New To Netflix?<span onClick={()=>{setSignState("sign up")}}>sign up now</span>
+            </p>
+          ) : (
+            <p>
+              Already have account? <span onClick={()=>{setSignState("sign in")}}>sign in now</span>
+              
+            </p>
+          )}
+        </div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
